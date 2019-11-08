da23d0dc736b446a0f2d2ab48b7dd6fc03af2e90
dinhorodrigues <39177938+dinhorodrigues@users.noreply.github.com>
2019-11-08 21:06:48 -0200

    melhoramento display e botoes

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import './Button.css';
 
 export default props =>
-    <button  className='button'>{props.label}</button>
+    <button  className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''} 
+    `}> 
+    {props.label}</button>/* se operation estiver definida coloca a classe operation caso contrario coloxa vazio,  usando
+    o dolar por que ta usando javaScript*/
